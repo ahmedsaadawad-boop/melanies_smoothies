565622550a4dbf6ed947df79125d87ee0f482e61
Ahmed Saad <ahmedsaadawad@gmail.com>
2026-05-24 15:59:22 +0300

    Fix API call and update subheader for fruit chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,8 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         # API Call
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        st.subheader(fruit_choosen + 'Nutrion Onfromation')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit" + fruit_choosen)  
         sf_df = st.dataframe(data=smoothiefroot_response.json())
       
     #st.write(ingredients_string)
