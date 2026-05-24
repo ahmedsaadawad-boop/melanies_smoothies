0cde67b5f6a0af3040fef7c376a73a0a4c75c532
Ahmed Saad <ahmedsaadawad@gmail.com>
2026-05-24 15:40:52 +0300

    Change API response display to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ if ingredients_list:
 # API Call
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json())
